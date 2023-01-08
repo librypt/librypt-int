e119a0a77cafd8fbaac5e3c2b54befb99ea0debc
Gabryx64 <gabryx86_64@riseup.net>
2023-01-08 04:55:18 +0100

    Implemented the Add and Sub trait in an unoptimized way

--- bitint_macro/src/lib.rs
+++ bitint_macro/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    parse_macro_input, punctuated::Punctuated, token::Paren, Field, Fields, FieldsUnnamed,
-    ItemStruct, LitInt, Path, PathArguments, PathSegment, Type, TypePath, Visibility,
+    parse_macro_input, punctuated::Punctuated, token::Paren, Field, Fields, FieldsUnnamed, Index,
+    ItemStruct, LitInt, Member, Path, PathArguments, PathSegment, Token, Type, TypePath, VisPublic,
+    Visibility,
 };
 
 use proc_macro2::{Ident, Span};
@@ -20,6 +21,7 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
 
     let mut punct = Punctuated::new();
 
+    let mut chunks = vec![];
     while bits != 0 {
         let intbits = if bits >= 128 {
             128
@@ -34,6 +36,7 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
         };
 
         bits -= intbits;
+        chunks.push(intbits);
         let mut segments = Punctuated::new();
         segments.push(PathSegment {
             ident: Ident::new(format!("u{}", intbits).as_str(), Span::call_site()),
@@ -42,7 +45,9 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
 
         punct.push(Field {
             attrs: vec![],
-            vis: Visibility::Inherited,
+            vis: Visibility::Public(VisPublic {
+                pub_token: Token![pub](Span::call_site()),
+            }),
             ident: None,
             colon_token: None,
             ty: Type::Path(TypePath {
@@ -55,6 +60,8 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
         });
     }
 
+    chunks.reverse();
+
     let input = ItemStruct {
         fields: Fields::Unnamed(FieldsUnnamed {
             paren_token: Paren {
@@ -65,8 +72,102 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
         ..input
     };
 
+    let name = input.ident.clone();
+
+    let add_quote = {
+        let idxs: Vec<_> = chunks
+            .iter()
+            .enumerate()
+            .rev()
+            .map(|(idx, _)| {
+                Member::Unnamed(Index {
+                    index: idx as u32,
+                    span: Span::call_site(),
+                })
+            })
+            .collect();
+
+        let types: Vec<_> = chunks
+            .iter()
+            .map(|x| Ident::new(format!("u{}", x).to_string().as_str(), Span::call_site()))
+            .collect();
+
+        let deftypes: Vec<_> = types.iter().rev().collect();
+
+        quote! {
+            let mut ret = Self(#(0 as #deftypes),*);
+            let mut carry = false;
+            let mut other_carry = false;
+
+            #(
+                (ret.#idxs, carry) = self.#idxs.overflowing_add(
+                    carry as #types + other_carry as #types
+                );
+                (ret.#idxs, other_carry) = ret.#idxs.overflowing_add(other.#idxs);
+            )*
+
+            assert!(!carry && !other_carry);
+
+            ret
+        }
+    };
+
+    let sub_quote = {
+        let idxs: Vec<_> = chunks
+            .iter()
+            .enumerate()
+            .rev()
+            .map(|(idx, _)| {
+                Member::Unnamed(Index {
+                    index: idx as u32,
+                    span: Span::call_site(),
+                })
+            })
+            .collect();
+
+        let types: Vec<_> = chunks
+            .iter()
+            .map(|x| Ident::new(format!("u{}", x).to_string().as_str(), Span::call_site()))
+            .collect();
+
+        let deftypes: Vec<_> = types.iter().rev().collect();
+
+        quote! {
+            let mut ret = Self(#(0 as #deftypes),*);
+            let mut carry = false;
+            let mut other_carry = false;
+
+            #(
+                (ret.#idxs, carry) = self.#idxs.overflowing_sub(
+                    carry as #types + other_carry as #types
+                );
+                (ret.#idxs, other_carry) = ret.#idxs.overflowing_sub(other.#idxs);
+            )*
+
+            assert!(!carry && !other_carry);
+
+            ret
+        }
+    };
+
     quote! {
         #input
+
+        impl std::ops::Add for #name {
+            type Output = Self;
+
+            fn add(self, other: Self) -> Self {
+                #add_quote
+            }
+        }
+
+        impl std::ops::Sub for #name {
+            type Output = Self;
+
+            fn sub(self, other: Self) -> Self {
+                #sub_quote
+            }
+        }
     }
     .into()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ macro_rules! define_multiple_uints {
         paste! {
             #[allow(non_camel_case_types)]
             #[bituint($x)]
-            #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+            #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
             pub struct [<u $x>];
         }
     };
@@ -17,7 +17,7 @@ macro_rules! define_multiple_uints {
         paste! {
             #[allow(non_camel_case_types)]
             #[bituint($x)]
-            #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+            #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
             pub struct [<u $x>];
         }
 
@@ -26,3 +26,13 @@ macro_rules! define_multiple_uints {
 }
 
 define_multiple_uints!(24, 48, 80, 256, 512, 1024, 2048, 4096);
+
+#[cfg(test)]
+mod tests {
+    use crate::u24;
+    #[test]
+    fn test() {
+        assert_eq!(u24(0, 5) + u24(0, 251), u24(1, 0));
+        assert_eq!(u24(1, 0) - u24(0, 251), u24(0, 5));
+    }
+}
