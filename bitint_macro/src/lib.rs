315191566b55b522082ff373eb52842fa6f8c1c2
Gabryx64 <gabryx86_64@riseup.net>
2023-01-09 15:29:12 +0100

    Moved add and sub impls to the overdlowing_* methods, added some other impls, conversion methods, AddAssign, SubAssign, and Add and Sub forward references

--- bitint_macro/src/lib.rs
+++ bitint_macro/src/lib.rs
@@ -10,9 +10,12 @@ use proc_macro2::{Ident, Span};
 
 #[proc_macro_attribute]
 pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
-    let mut bits: usize = parse_macro_input!(arg as LitInt).base10_parse().unwrap();
-    assert!(bits % 8 == 0, "Bits needs to be 8 aligned");
-    let mut input = parse_macro_input!(input as ItemStruct);
+    let bits: u32 = parse_macro_input!(arg as LitInt).base10_parse().unwrap();
+    assert!(
+        bits % 8 == 0 && bits != 0,
+        "Bits need to be 8 aligned and non zero"
+    );
+    let input = parse_macro_input!(input as ItemStruct);
 
     if let Fields::Unit = input.fields {
     } else {
@@ -22,42 +25,45 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
     let mut punct = Punctuated::new();
 
     let mut chunks = vec![];
-    while bits != 0 {
-        let intbits = if bits >= 128 {
-            128
-        } else if bits >= 64 {
-            64
-        } else if bits >= 32 {
-            32
-        } else if bits >= 16 {
-            16
-        } else {
-            8
-        };
+    {
+        let mut bits = bits;
+        while bits != 0 {
+            let intbits = if bits >= 128 {
+                128
+            } else if bits >= 64 {
+                64
+            } else if bits >= 32 {
+                32
+            } else if bits >= 16 {
+                16
+            } else {
+                8
+            };
 
-        bits -= intbits;
-        chunks.push(intbits);
-        let mut segments = Punctuated::new();
-        segments.push(PathSegment {
-            ident: Ident::new(format!("u{}", intbits).as_str(), Span::call_site()),
-            arguments: PathArguments::None,
-        });
+            bits -= intbits;
+            chunks.push(intbits);
+            let mut segments = Punctuated::new();
+            segments.push(PathSegment {
+                ident: Ident::new(format!("u{}", intbits).as_str(), Span::call_site()),
+                arguments: PathArguments::None,
+            });
 
-        punct.push(Field {
-            attrs: vec![],
-            vis: Visibility::Public(VisPublic {
-                pub_token: Token![pub](Span::call_site()),
-            }),
-            ident: None,
-            colon_token: None,
-            ty: Type::Path(TypePath {
-                qself: None,
-                path: Path {
-                    leading_colon: None,
-                    segments,
-                },
-            }),
-        });
+            punct.push(Field {
+                attrs: vec![],
+                vis: Visibility::Public(VisPublic {
+                    pub_token: Token![pub](Span::call_site()),
+                }),
+                ident: None,
+                colon_token: None,
+                ty: Type::Path(TypePath {
+                    qself: None,
+                    path: Path {
+                        leading_colon: None,
+                        segments,
+                    },
+                }),
+            });
+        }
     }
 
     chunks.reverse();
@@ -74,98 +80,352 @@ pub fn bituint(arg: TokenStream, input: TokenStream) -> TokenStream {
 
     let name = input.ident.clone();
 
-    let add_quote = {
-        let idxs: Vec<_> = chunks
-            .iter()
-            .enumerate()
-            .rev()
-            .map(|(idx, _)| {
-                Member::Unnamed(Index {
-                    index: idx as u32,
-                    span: Span::call_site(),
-                })
+    let idxs: Vec<_> = chunks
+        .iter()
+        .enumerate()
+        .rev()
+        .map(|(idx, _)| {
+            Member::Unnamed(Index {
+                index: idx as u32,
+                span: Span::call_site(),
             })
-            .collect();
+        })
+        .collect();
 
-        let types: Vec<_> = chunks
-            .iter()
-            .map(|x| Ident::new(format!("u{}", x).to_string().as_str(), Span::call_site()))
-            .collect();
+    let types: Vec<_> = chunks
+        .iter()
+        .map(|x| Ident::new(format!("u{}", x).to_string().as_str(), Span::call_site()))
+        .collect();
 
-        let deftypes: Vec<_> = types.iter().rev().collect();
+    let last_member = Member::Unnamed(Index {
+        index: chunks.len() as u32 - 1,
+        span: Span::call_site(),
+    });
+    let last_member_type = types.first().unwrap().clone();
 
-        quote! {
-            let mut ret = Self(#(0 as #deftypes),*);
-            let mut carry = false;
-            let mut other_carry = false;
+    let add_quote = quote! {
+        let mut ret = #name::MIN;
+        let mut carry = false;
+        let mut other_carry = false;
 
-            #(
-                (ret.#idxs, carry) = self.#idxs.overflowing_add(
-                    carry as #types + other_carry as #types
-                );
-                (ret.#idxs, other_carry) = ret.#idxs.overflowing_add(other.#idxs);
-            )*
+        #(
+            (ret.#idxs, carry) = self.#idxs.overflowing_add(
+                carry as #types + other_carry as #types
+            );
+            (ret.#idxs, other_carry) = ret.#idxs.overflowing_add(rhs.#idxs);
+        )*
 
-            assert!(!carry && !other_carry);
-
-            ret
+        if carry || other_carry {
+            ret = #name::MIN;
+            ret.#last_member = carry as #last_member_type + other_carry as #last_member_type;
         }
+
+        (ret, carry || other_carry)
     };
 
-    let sub_quote = {
-        let idxs: Vec<_> = chunks
-            .iter()
-            .enumerate()
-            .rev()
-            .map(|(idx, _)| {
-                Member::Unnamed(Index {
-                    index: idx as u32,
-                    span: Span::call_site(),
-                })
-            })
-            .collect();
+    let sub_quote = quote! {
+        let mut ret = #name::MIN;
+        let mut carry = false;
+        let mut other_carry = false;
 
-        let types: Vec<_> = chunks
-            .iter()
-            .map(|x| Ident::new(format!("u{}", x).to_string().as_str(), Span::call_site()))
-            .collect();
+        #(
+            (ret.#idxs, carry) = self.#idxs.overflowing_sub(
+                carry as #types + other_carry as #types
+            );
+            (ret.#idxs, other_carry) = ret.#idxs.overflowing_sub(rhs.#idxs);
+        )*
 
-        let deftypes: Vec<_> = types.iter().rev().collect();
-
-        quote! {
-            let mut ret = Self(#(0 as #deftypes),*);
-            let mut carry = false;
-            let mut other_carry = false;
-
-            #(
-                (ret.#idxs, carry) = self.#idxs.overflowing_sub(
-                    carry as #types + other_carry as #types
-                );
-                (ret.#idxs, other_carry) = ret.#idxs.overflowing_sub(other.#idxs);
-            )*
-
-            assert!(!carry && !other_carry);
-
-            ret
+        if carry || other_carry {
+            ret = #name::MAX;
+            ret.#last_member -= carry as #last_member_type + other_carry as #last_member_type;
         }
+
+        (ret, carry || other_carry)
     };
+
+    let deftypes: Vec<_> = types.iter().rev().collect();
 
     quote! {
         #input
 
-        impl std::ops::Add for #name {
-            type Output = Self;
+        impl #name {
+            const MIN: #name = #name(#(#deftypes::MIN),*);
+            const MAX: #name = #name(#(#deftypes::MAX),*);
+            const BITS: u32 = #bits;
 
-            fn add(self, other: Self) -> Self {
-                #add_quote
+            pub const fn overflowing_add(self, rhs: #name) -> (#name, bool) {
+               #add_quote
+            }
+
+            pub const fn overflowing_sub(self, rhs: #name) -> (#name, bool) {
+                #sub_quote
+            }
+        }
+
+        impl std::ops::Add for #name {
+            type Output = #name;
+
+            #[inline]
+            fn add(self, other: #name) -> #name {
+                let (ret, carry) = self.overflowing_add(other);
+
+                debug_assert!(!carry, "attempt to add with overflow");
+
+                ret
+            }
+        }
+
+        impl std::ops::AddAssign for #name {
+            #[inline]
+            fn add_assign(&mut self, other: #name) {
+                *self = *self + other;
             }
         }
 
         impl std::ops::Sub for #name {
-            type Output = Self;
+            type Output = #name;
 
-            fn sub(self, other: Self) -> Self {
-                #sub_quote
+            #[inline]
+            fn sub(self, other: #name) -> #name {
+                let (ret, carry) = self.overflowing_sub(other);
+
+                debug_assert!(!carry, "attempt to subtract with overflow");
+
+                ret
+            }
+        }
+
+        impl std::ops::SubAssign for #name {
+            #[inline]
+            fn sub_assign(&mut self, other: #name) {
+                *self = *self - other;
+            }
+        }
+
+        impl std::ops::Add<&#name> for &#name {
+            type Output = #name;
+
+            #[inline]
+            fn add(self, other: &#name) -> #name {
+                *self + *other
+            }
+        }
+
+
+        impl std::ops::Sub<&#name> for &#name {
+            type Output = #name;
+
+            #[inline]
+            fn sub(self, other: &#name) -> #name {
+                *self - *other
+            }
+        }
+
+
+        impl std::ops::Add<&#name> for #name {
+            type Output = #name;
+
+            #[inline]
+            fn add(self, other: &#name) -> #name {
+                self + *other
+            }
+        }
+
+        impl std::ops::AddAssign<&#name> for #name {
+            #[inline]
+            fn add_assign(&mut self, other: &#name) {
+                *self = *self + other;
+            }
+        }
+
+        impl std::ops::Sub<&#name> for #name {
+            type Output = #name;
+
+            #[inline]
+            fn sub(self, other: &#name) -> #name {
+                self - *other
+            }
+        }
+
+        impl std::ops::SubAssign<&#name> for #name {
+            #[inline]
+            fn sub_assign(&mut self, other: &#name) {
+                *self = *self - other;
+            }
+        }
+
+        impl<'a> std::ops::Add<#name> for &'a #name {
+            type Output = #name;
+
+            #[inline]
+            fn add(self, other: #name) -> #name {
+                *self + other
+            }
+        }
+
+        impl<'a> std::ops::Sub<#name> for &'a #name {
+            type Output = #name;
+
+            #[inline]
+            fn sub(self, other: #name) -> #name {
+                *self - other
+            }
+        }
+
+        impl Default for #name {
+            #[inline]
+            fn default() -> #name {
+                #name::MIN
+            }
+        }
+
+        impl From<u8> for #name {
+            #[inline]
+            fn from(value: u8) -> #name {
+                let mut ret = #name::MIN;
+                ret.#last_member = value as #last_member_type;
+                ret
+            }
+        }
+
+        impl From<u16> for #name {
+            #[inline]
+            fn from(value: u16) -> #name {
+                let mut ret = #name::MIN;
+                ret.#last_member = value as #last_member_type;
+                ret
+            }
+        }
+
+        impl From<u32> for #name {
+            #[inline]
+            fn from(value: u32) -> #name {
+                let mut ret = #name::MIN;
+                ret.#last_member = value as #last_member_type;
+                ret
+            }
+        }
+
+        impl From<u64> for #name {
+            #[inline]
+            fn from(value: u64) -> #name {
+                let mut ret = #name::MIN;
+                ret.#last_member = value as #last_member_type;
+                ret
+            }
+        }
+
+        impl From<u128> for #name {
+            #[inline]
+            fn from(value: u128) -> #name {
+                let mut ret = #name::MIN;
+                ret.#last_member = value as #last_member_type;
+                ret
+            }
+        }
+
+        impl From<i8> for #name {
+            #[inline]
+            fn from(value: i8) -> #name {
+                (value as u128).into()
+            }
+        }
+
+        impl From<i16> for #name {
+            #[inline]
+            fn from(value: i16) -> #name {
+                (value as u128).into()
+            }
+        }
+
+        impl From<i32> for #name {
+            #[inline]
+            fn from(value: i32) -> #name {
+                (value as u128).into()
+            }
+        }
+
+        impl From<i64> for #name {
+            #[inline]
+            fn from(value: i64) -> #name {
+                (value as u128).into()
+            }
+        }
+
+        impl From<i128> for #name {
+            #[inline]
+            fn from(value: i128) -> #name {
+                (value as u128).into()
+            }
+        }
+
+        impl From<#name> for u8 {
+            #[inline]
+            fn from(value: #name) -> u8 {
+                value.#last_member as u8
+            }
+        }
+
+        impl From<#name> for u16 {
+            #[inline]
+            fn from(value: #name) -> u16 {
+                value.#last_member as u16
+            }
+        }
+
+        impl From<#name> for u32 {
+            #[inline]
+            fn from(value: #name) -> u32 {
+                value.#last_member as u32
+            }
+        }
+
+        impl From<#name> for u64 {
+            #[inline]
+            fn from(value: #name) -> u64 {
+                value.#last_member as u64
+            }
+        }
+
+        impl From<#name> for u128 {
+            #[inline]
+            fn from(value: #name) -> u128 {
+                value.#last_member as u128
+            }
+        }
+
+        impl From<#name> for i8 {
+            #[inline]
+            fn from(value: #name) -> i8 {
+                u128::from(value) as i8
+            }
+        }
+
+        impl From<#name> for i16 {
+            #[inline]
+            fn from(value: #name) -> i16 {
+                u128::from(value) as i16
+            }
+        }
+
+        impl From<#name> for i32 {
+            #[inline]
+            fn from(value: #name) -> i32 {
+                u128::from(value) as i32
+            }
+        }
+
+        impl From<#name> for i64 {
+            #[inline]
+            fn from(value: #name) -> i64 {
+                u128::from(value) as i64
+            }
+        }
+
+        impl From<#name> for i128 {
+            #[inline]
+            fn from(value: #name) -> i128 {
+                u128::from(value) as i128
             }
         }
     }
